9a9f33c7f547f563154b64173e1c869823821144
midhyun <kfree912@gmail.com>
2022-11-01 17:50:54 +0900

    reviews_index

--- pjt/settings.py
+++ pjt/settings.py
@@ -31,10 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "imagekit",
     "reviews",
     "accounts",
     "django_bootstrap5",
-    "imagekit",
     "django_extensions",
     "django.contrib.admin",
     "django.contrib.auth",
--- reviews/urls.py
+++ reviews/urls.py
@@ -4,10 +4,10 @@ from . import views
 app_name = 'reviews'
 
 urlpatterns = [
-    path('', views.main, name='main'),
+    path('', views.main, name='index'),
     path('hotlist', views.hotlist, name='hotlist'),
     path('<int:pk>', views.hotdetail, name='hotdetail'),
     path('hotcreate', views.hotcreate, name='hotcreate'),
     path('<int:pk>/reviewcreate', views.reviewcreate, name='reviewcreate'),
-    path('<int:pk>/delete', views.delete, name='delete')
+    path('<int:pk>/delete', views.delete, name='delete'),
 ]
